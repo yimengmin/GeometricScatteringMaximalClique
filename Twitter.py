791d64598b6e718d8d9457f7ed98a9da52429627
yimengmin <yimengmin@outlook.com>
2022-07-09 02:08:41 -0400

    update

--- Twitter.py
+++ Twitter.py
@@ -35,6 +35,7 @@ parser.add_argument('--batch_size', type=int, default=256,
                     help='batch_size')
 parser.add_argument('--nlayers', type=int, default=4,
                     help='num of layers')
+parser.add_argument('--use_smoo', action='store_true')
 parser.add_argument('--EPOCHS', type=int, default=20,
                     help='epochs to train')
 parser.add_argument('--penalty_coefficient', type=float, default=2.,
@@ -87,7 +88,8 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 from models import GNN,GCN
 #scattering model
-model = GNN(input_dim=3, hidden_dim=args.hidden, output_dim=1, n_layers=args.nlayers,dropout=args.dropout,Withgres=False,smooth=args.smoo)
+#model = GNN(input_dim=3, hidden_dim=args.hidden, output_dim=1, n_layers=args.nlayers,dropout=args.dropout,Withgres=False,smooth=args.smoo)
+model = GNN(input_dim=3, hidden_dim=args.hidden, output_dim=1, n_layers=args.nlayers,dropout=args.dropout,Withgres=args.use_smoo,smooth=args.smoo)
 #model = GCN(input_dim=3, hidden_dim=args.hidden, output_dim=1, dropout=args.dropout)
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
