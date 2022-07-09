b608951c6ae79dc83965f749b7b781d0ccfe158a
yimengmin <yimengmin@outlook.com>
2022-07-09 01:33:25 -0400

    update

--- layers.py
+++ layers.py
@@ -42,6 +42,7 @@ class GC(Module):
                + str(self.out_features) + ')'
 
 
+
 class GC_withres(Module):
     """
     res conv
@@ -51,32 +52,44 @@ class GC_withres(Module):
         self.in_features = in_features
         self.out_features = out_features
         self.smooth = smooth
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-    def reset_parameters(self):
-        nn.init.xavier_uniform_(self.weight.data, gain=1.414)
-        stdv = 1. / math.sqrt(self.weight.size(1))
+#        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+#        if bias:
+#            self.bias = Parameter(torch.FloatTensor(out_features))
+#        else:
+#            self.register_parameter('bias', None)
+#        self.reset_parameters()
+        self.mlp = nn.Linear(in_features, out_features)
+#    def reset_parameters(self):
+#        nn.init.xavier_uniform_(self.weight.data, gain=1.414)
+#        stdv = 1. / math.sqrt(self.weight.size(1))
 #        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
+#        if self.bias is not None:
+#            self.bias.data.uniform_(-stdv, stdv)
     def forward(self, input, adj):
         # adj is extracted from the graph structure
-        support = torch.mm(input, self.weight)
-        I_n = sp.eye(adj.shape[0])
+        adj = adj.to_torch_sparse_coo_tensor()
+#        support = torch.mm(input, self.weight)
+        support = self.mlp(input)
+        I_n = sp.eye(adj.size(0))
         I_n = sparse_mx_to_torch_sparse_tensor(I_n).cuda()
-        output = torch.spmm((I_n+self.smooth*adj)/(1+self.smooth), support)
-        if self.bias is not None:
-            return output + self.bias
-        else:
-            return output
+        A_gcn = adj +  I_n
+        degrees = torch.sparse.sum(A_gcn,0)
+        D = degrees
+        D = D.to_dense() # transfer D from sparse tensor to normal torch tensor
+        D = torch.pow(D, -0.5)
+        D = D.unsqueeze(dim=1)
+        A_gcn_feature = support
+        A_gcn_feature = torch.mul(A_gcn_feature,D)
+        A_gcn_feature = torch.spmm(A_gcn,A_gcn_feature)
+        A_gcn_feature = torch.mul(A_gcn_feature,D)
+        output = A_gcn_feature * self.smooth + support
+        output = output/(1+self.smooth)
+#        if self.bias is not None:
+#            return output + self.bias
+#        else:
+#            return output
+        return output
     def __repr__(self):
         return self.__class__.__name__ + ' (' \
                + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
-
-
 
