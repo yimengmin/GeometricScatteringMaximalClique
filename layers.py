3b40bd933fca9c336b7756b54c52e8ed7e36d5ad
Yimeng <16371594+yimengmin@users.noreply.github.com>
2023-08-23 00:44:10 -0400

    Update layers.py
    clean and update

--- layers.py
+++ layers.py
@@ -22,8 +22,6 @@ class GC(Module):
         # adj is extracted from the graph structure
         support = self.mlp(input)
 
-#        adj = adj.to_torch_sparse_coo_tensor()
-#        support = torch.mm(input, self.weight)
         I_n = sp.eye(adj.size(0))
         I_n = sparse_mx_to_torch_sparse_tensor(I_n).to(device)
         A_gcn = adj +  I_n
@@ -49,23 +47,8 @@ class GC_withres(Module):
         self.in_features = in_features
         self.out_features = out_features
         self.smooth = smooth
-#        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-#        if bias:
-#            self.bias = Parameter(torch.FloatTensor(out_features))
-#        else:
-#            self.register_parameter('bias', None)
-#        self.reset_parameters()
         self.mlp = nn.Linear(in_features, out_features)
-#    def reset_parameters(self):
-#        nn.init.xavier_uniform_(self.weight.data, gain=1.414)
-#        stdv = 1. / math.sqrt(self.weight.size(1))
-#        self.weight.data.uniform_(-stdv, stdv)
-#        if self.bias is not None:
-#            self.bias.data.uniform_(-stdv, stdv)
     def forward(self, input, adj,device='cuda'):
-        # adj is extracted from the graph structure
-#        adj = adj.to_torch_sparse_coo_tensor()
-#        support = torch.mm(input, self.weight)
         support = self.mlp(input)
         I_n = sp.eye(adj.size(0))
         I_n = sparse_mx_to_torch_sparse_tensor(I_n).to(device)
